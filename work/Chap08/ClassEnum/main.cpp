03876faa5f5fbcc983858684f5124a5058a34b20
Andrew C. Scoppa <andrew-scoppa@github.com>
2024-04-14 11:37:17 -0400

    incremental

--- work/Chap08/ClassEnum/main.cpp
+++ work/Chap08/ClassEnum/main.cpp
@@ -1,43 +1,65 @@
 #include <iostream>
 
-using namespace std;
-
-enum class Font{
-    fancy = 0,
+enum class FontFamily
+{
+    classic = 0,
     stylish = 2,
-    boring = 3
+    fancy = 3
 };
 
-enum Permissions : unsigned
+enum class Style : unsigned short int
 {
     None = 0,
-    Owner = 0x2,
-    Group = 0x4,
-    Public = 0x8
+    bold = 0x2,
+    italic = 0x4,
+    underline = 0x8
 };
 
-void DisplayWelcome(string Message, Font font)
+bool CheckFontSupport(FontFamily font = FontFamily::classic, unsigned short int style = 0)
 {
+    bool isSupported = true;
+    bool isBoldItalic =  false;
     switch (font)
     {
-    case Font::fancy :
-        /* code for fancy*/
+    case FontFamily::fancy:
+        // error if the font is fancy and the style is bold and italic
+        isBoldItalic = (style == (unsigned short int)Style::bold) & ((unsigned short int)Style::italic);
+        if (isBoldItalic)
+        {
+            std::clog << "Error: Fancy font cannot be bold italic." << std::endl;
+            isSupported = false;
+        }
         break;
-    case Font::boring :
-        /* code for boring*/
-        break;
-    case Font::stylish :
-        /* code stylish*/
+    case FontFamily::classic:
+        // error if the font is classic and the style is undeline
+        if (style == (unsigned short int)Style::underline)
+        {
+            std::clog << "Error: Classic font cannot be underlined." << std::endl;
+            isSupported = false;
+        }
         break;
     default:
         /* otherwise*/
         break;
     }
+
+    return isSupported;
 }
 
 int main()
 {
-    DisplayWelcome("Hello World!", Font::fancy);
-    unsigned userPerms = Owner | Group;
 
+    // check if the font family fancy supports the style  bold italic
+    if (CheckFontSupport(FontFamily::fancy, (unsigned short int)Style::bold | (unsigned short int)Style::italic))
+    {
+        std::clog << "Fancy font supports the style bold italic." << std::endl;
+    }
+
+    // check if the font family classic supports the style underline
+    if (CheckFontSupport(FontFamily::classic, (unsigned short int)Style::underline))
+    {
+        std::clog << "Classic font supports the style." << std::endl;
+    }
+
+    return 0;
 }
