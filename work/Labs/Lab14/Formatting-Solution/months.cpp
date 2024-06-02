ff3d2c97282f035bca07c6c97e93120eb19e7e10
Andrew C. Scoppa <andrew-scoppa@github.com>
2024-06-02 07:24:48 -0400

    Sync with remote repository

--- work/Labs/Lab14/Formatting-Solution/months.cpp
+++ work/Labs/Lab14/Formatting-Solution/months.cpp
@@ -15,12 +15,10 @@ int main()
 	int total = 0;
 	for (int i = 0; i < 12; ++i)
 	{
-		//cout << months[i] << "  " << days[i] << endl;
 		cout << left << setw(15) << months[i]
 			 << right << setw(3) << days[i] << endl;
 		total += days[i];
 	}
-	//cout << "Total days = " << total << endl;
 	cout << left << setw(15) << "Total Days = "
 		 << right << setw(3) << total << endl;
 
